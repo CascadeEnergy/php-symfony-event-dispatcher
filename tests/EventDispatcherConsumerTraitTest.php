5d72248c706f487355192464681f381b410bf95d
Charles Berube <charles.berube@cascadeenergy.com>
2015-07-30 11:19:22 -0700

    Remove extra 'use' statement

--- tests/EventDispatcherConsumerTraitTest.php
+++ tests/EventDispatcherConsumerTraitTest.php
@@ -3,7 +3,6 @@
 namespace CascadeEnergy\Tests\SymfonyEventDispatcher;
 
 use CascadeEnergy\SymfonyEventDispatcher\EventDispatcherConsumerTrait;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class EventDispatcherConsumerTraitTest extends \PHPUnit_Framework_TestCase
 {
